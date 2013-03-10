346f3af8093dedca2e2f841b1e2d90ef417bead6
dsblue <dsblue@gmail.com>
2013-03-10 17:26:53 -0500

    Bluetooth working with Hello World output

--- src/com/example/btconsole/ConfigureActivity.java
+++ src/com/example/btconsole/ConfigureActivity.java
@@ -1,16 +1,63 @@
 package com.example.btconsole;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ConfigureActivity extends Activity 
 	implements ConfigureFragment.OnFragmentInteractionListener {
 
+	public static final String NAME = "TEST";
+	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+	
+	private BluetoothAdapter mBluetoothAdapter;
+	private ConnectThread connectThread;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_configure);
+		
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+		    // Device does not support Bluetooth
+		}
+		
+		/*if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}*/
+		
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+		// If there are paired devices
+		if (pairedDevices.size() > 0) {
+		    // Loop through paired devices
+		    for (BluetoothDevice device : pairedDevices) {
+		        // Add the name and address to an array adapter to show in a ListView
+		    	BluetoothClass btc = device.getBluetoothClass();
+		    	Log.d("BT",device.getName() + "\n" + device.getAddress() + "\n" + btc.getDeviceClass() + "\n" + btc.getMajorDeviceClass() + "\n");
+//		        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+		    	
+		    	if (device.getAddress().equalsIgnoreCase("00:12:6f:21:D6:09") ) {
+		    		Log.d("BT", "Connecting to: " + device.getAddress() + "\n");
+		    		connectThread = new ConnectThread(device);
+		    	}
+		    	
+		    }
+		}
+		
+		connectThread.run();
 	}
 
 	@Override
@@ -27,5 +74,74 @@ public class ConfigureActivity extends Activity
 		return true;
 	}
 	*/
+	private class ConnectThread extends Thread {
+	    private final BluetoothSocket mmSocket;
+	    private final BluetoothDevice mmDevice;
+	 
+	    public ConnectThread(BluetoothDevice device) {
+	        // Use a temporary object that is later assigned to mmSocket,
+	        // because mmSocket is final
+	        BluetoothSocket tmp = null;
+	        mmDevice = device;
+	 
+	        // Get a BluetoothSocket to connect with the given BluetoothDevice
+	        try {
+	            // MY_UUID is the app's UUID string, also used by the server code
+	        	Log.d("BT", "RFCOMM: " + "\n");
+	            tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+	        } catch (IOException e) { }
+	        mmSocket = tmp;
+	    }
+	 
+	    public void run() {
+	        // Cancel discovery because it will slow down the connection
+	        mBluetoothAdapter.cancelDiscovery();
+	 
+	        Log.d("BT", "Running thread" + "\n");
+	        
+	        try {
+	            // Connect the device through the socket. This will block
+	            // until it succeeds or throws an exception
+	        	Log.d("BT", "Connecting" + "\n");
+	            mmSocket.connect();
+	            Log.d("BT", "Connected " + "\n");
+	        } catch (IOException connectException) {
+	            // Unable to connect; close the socket and get out
+	            try {
+	            	Log.d("BT", "Could not connect... " + "\n");
+	                mmSocket.close();
+	            } catch (IOException closeException) { }
+	            return;
+	        }
+	 
+	        // Do work to manage the connection (in a separate thread)
+	        //manageConnectedSocket(mmSocket);
+	        
+	        InputStream tmpIn = null;
+	        OutputStream tmpOut = null;
+	 
+	        // Get the input and output streams, using temp objects because
+	        // member streams are final
+	        try {
+	            tmpIn = mmSocket.getInputStream();
+	            tmpOut = mmSocket.getOutputStream();
+	        } catch (IOException e) { }
+	       
+	        try {
+				tmpOut.write("Hello World 2\n\r".getBytes());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
+	    }
+	 
+	    /** Will cancel an in-progress connection, and close the socket */
+	    public void cancel() {
+	        try {
+	            mmSocket.close();
+	        } catch (IOException e) { }
+	    }
+	}
+	
 }
