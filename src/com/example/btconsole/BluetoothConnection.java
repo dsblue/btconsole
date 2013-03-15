ad3da29fdee62a006ffccdaaefc6bc84b9530c96
dsblue <dsblue@gmail.com>
2013-03-14 21:14:30 -0500

    Fixed Bluetoth Connect thread

--- src/com/example/btconsole/BluetoothConnection.java
+++ src/com/example/btconsole/BluetoothConnection.java
@@ -3,23 +3,29 @@ package com.example.btconsole;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
-public class BluetoothConnection extends SerialConnection {
-
+public class BluetoothConnection extends SerialConnection implements Handler.Callback{
+	
 	private static final UUID SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 	private static final int REQUEST_ENABLE_BT = 0;
-
+	private static final int MESSAGE_READ = 1;
+	
 	private BluetoothAdapter mBluetoothAdapter;
 	private BluetoothDevice mDevice;
 	
 	private ConnectThread connectThread;	
+	
+	final Handler mHandler = new Handler(this);
 	
 	BluetoothConnection(BluetoothAdapter adapter, BluetoothDevice device){
 		super(device.getName(), device.getAddress());
@@ -40,7 +46,7 @@ public class BluetoothConnection extends SerialConnection {
 		
 		connectThread = new ConnectThread(mDevice);
 		
-		connectThread.run();
+		connectThread.start();
 		
 		return true;
 	}
@@ -95,23 +101,8 @@ public class BluetoothConnection extends SerialConnection {
 	        // Do work to manage the connection (in a separate thread)
 	        //manageConnectedSocket(mmSocket);
 	        
-	        InputStream tmpIn = null;
-	        OutputStream tmpOut = null;
-	 
-	        // Get the input and output streams, using temp objects because
-	        // member streams are final
-	        try {
-	            tmpIn = mmSocket.getInputStream();
-	            tmpOut = mmSocket.getOutputStream();
-	        } catch (IOException e) { }
-	       
-	        try {
-				tmpOut.write("Hello World 2\n\r".getBytes());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
+	        new ConnectedThread(mmSocket).start();
+	        
 	    }
 	 
 	    /** Will cancel an in-progress connection, and close the socket */
@@ -120,6 +111,81 @@ public class BluetoothConnection extends SerialConnection {
 	            mmSocket.close();
 	        } catch (IOException e) { }
 	    }
+	}
+	
+	private class ConnectedThread extends Thread {
+	    private final BluetoothSocket mmSocket;
+	    private final InputStream mmInStream;
+	    private final OutputStream mmOutStream;
+	 
+	    public ConnectedThread(BluetoothSocket socket) {
+	        mmSocket = socket;
+	        InputStream tmpIn = null;
+	        OutputStream tmpOut = null;
+	 
+	        // Get the input and output streams, using temp objects because
+	        // member streams are final
+	        try {
+	            tmpIn = socket.getInputStream();
+	            tmpOut = socket.getOutputStream();
+	        } catch (IOException e) { }
+	 
+	        mmInStream = tmpIn;
+	        mmOutStream = tmpOut;
+	    }
+	 
+	    public void run() {
+	        byte[] buffer = new byte[1024];  // buffer store for the stream
+	        int bytes; // bytes returned from read()
+	 
+	        // Keep listening to the InputStream until an exception occurs
+	        while (true) {
+	            try {
+	                // Read from the InputStream
+	                bytes = mmInStream.read(buffer);
+	                // Send the obtained bytes to the UI activity
+	                mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+	                        .sendToTarget();
+	            } catch (IOException e) {
+	                break;
+	            }
+	        }
+	    }
+	 
+	    /* Call this from the main activity to send data to the remote device */
+	    public void write(byte[] bytes) {
+	        try {
+	            mmOutStream.write(bytes);
+	        } catch (IOException e) { }
+	    }
+	 
+	    /* Call this from the main activity to shutdown the connection */
+	    public void cancel() {
+	        try {
+	            mmSocket.close();
+	        } catch (IOException e) { }
+	    }
+	}
+
+	@Override
+	public boolean handleMessage(Message msg) {
+		byte[] buffer;
+		int len;
+		
+		if (msg.what == MESSAGE_READ) {
+			buffer = (byte[])msg.obj;
+			len = msg.arg1;
+			
+			buffer[len] = 0;
+			try {
+				Log.d("BT",new String(buffer, "UTF-8").substring(0, len));
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return true;
 	}
 	
 	
